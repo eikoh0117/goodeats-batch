e20e4e6c3cdb1457a4d07ed2e156fe4eaf7d7089
Hidemitsu Aoki <olympos0117@gmail.com>
2021-01-11 16:35:10 +0900

    Define get_place_id

---
+++ lambda_function.rb
@@ -0,0 +1,38 @@
+require 'net/http'
+require 'uri'
+require 'json'
+require 'dotenv/load'
+require 'aws-record'
+
+
+class GoodEats
+  include Aws::Record
+  integer_attr :id, hash_key: true
+  integer_attr :restaurant_id
+  integer_attr :name
+  integer_attr :comment
+  integer_attr :evaluation
+  integer_attr :photo
+  integer_attr :date
+end
+
+module Area
+  # ぐるなびAPIで定められているエリアコード（AREAS2156: 池袋東口・東池袋）
+  CODES = ['AREAS2115']
+end
+
+def get_place_id(restaurant_name)
+  # Google Places APIを使って口コミの情報を取得
+  resource = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=#{restaurant_name}&key=#{ENV['GOOGLE_MAP_API_KEY']}&language=ja"
+  enc_str = URI.encode(resource)
+  uri = URI.parse(enc_str)
+  json = Net::HTTP.get(uri)
+  places = JSON.parse(json)
+  return unless places['results'].first # 店舗が存在しないときはnilを返す
+  places['results'][0]['place_id']
+end
+
+def get_reviews
+end
+
+get_place_id("磯丸水産")
