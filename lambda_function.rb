a7a3dbe51cda67428e3083d564db22b80dbc0c0f
Hidemitsu Aoki <olympos0117@gmail.com>
2021-01-11 16:51:03 +0900

    Define fetch_data

--- lambda_function.rb
+++ lambda_function.rb
@@ -21,18 +21,22 @@ module Area
   CODES = ['AREAS2115']
 end
 
-def get_place_id(restaurant_name)
-  # Google Places APIを使って口コミの情報を取得
-  resource = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=#{restaurant_name}&key=#{ENV['GOOGLE_MAP_API_KEY']}&language=ja"
+def fetch_data(resource)
   enc_str = URI.encode(resource)
   uri = URI.parse(enc_str)
   json = Net::HTTP.get(uri)
-  places = JSON.parse(json)
+  JSON.parse(json)
+end
+
+def get_place_id(restaurant_name)
+  # Google Places APIを使って口コミの情報を取得
+  resource = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=#{restaurant_name}&key=#{ENV['GOOGLE_MAP_API_KEY']}&language=ja"
+  places = fetch_data(resource)
   return unless places['results'].first # 店舗が存在しないときはnilを返す
   places['results'][0]['place_id']
 end
 
-def get_reviews
+def get_reviews(place_id)
 end
 
 get_place_id("磯丸水産")
