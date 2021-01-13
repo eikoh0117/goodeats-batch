65d75a393c69fc377ab8009d93653c838dbc414c
Hidemitsu Aoki <olympos0117@gmail.com>
2021-01-13 23:38:35 +0900

    Define module Category

--- lambda_function.rb
+++ lambda_function.rb
@@ -7,7 +7,8 @@ require 'aws-record'
 
 class GoodEatsReviews
   include Aws::Record
-  integer_attr :id, hash_key: true
+  integer_attr :restaurant_id, hash_key: true
+  integer_attr :place_id, hash_key: true
   string_attr :author_name
   integer_attr :rating
   string_attr :text
@@ -16,8 +17,16 @@ end
 
 module Area
   # ぐるなびAPIで定められているエリアコード（AREAS2156: 池袋東口・東池袋）AREAS5566(月寒)
-  CODES = ['AREAS5566']
+  # CODES = ['AREAS5566']
+  CODES = ['AREAS2115']
 end
+
+module Category
+  # ぐるなびAPIで定められている大業態コード
+  CODES = ['RSFST09000', 'RSFST02000', 'RSFST03000', 'RSFST04000', 'RSFST05000', 'RSFST06000', 'RSFST01000', 'RSFST07000', 'RSFST08000', 'RSFST14000', 'RSFST11000', 'RSFST13000', 'RSFST12000', 'RSFST16000', 'RSFST15000', 'RSFST17000', 'RSFST10000', 'RSFST21000', 'RSFST18000', 'RSFST19000', 'RSFST20000', 'RSFST90000']
+end
+
+def 
 
 def fetch_data(resource)
   enc_str = URI.encode(resource)
@@ -43,10 +52,11 @@ def get_reviews(place_id)
   reviews
 end
 
-def put_item(restaurant_id, review) # DynamoDBへ保存
+def put_item(restaurant_id, place_idreview) # DynamoDBへ保存
   return if GoodEatsReviews.find(id: restaurant_id, author_name: review['author_name']) # 既にDynamoDBに同じIDのレコードが存在した場合は新たに保存しない
   review = GoodEatsReviews.new
-  review.id = restaurant_id
+  review.restaurant_id = restaurant_id
+  review.place_id = place_id
   review.author_name = review['author_name']
   review.rating = review['rating']
   review.text = review['text']
@@ -56,11 +66,52 @@ end
 
 def lambda_handler
   Area::CODES.each do |code|
-    resource = "https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=#{ENV['GNAVI_API_KEY']}&areacode_s=#{code}"
-    restaurants = fetch_data(resource)
-    restaurants.each do |restaurant|
-      puts restaurant['rest']
+    restaurants = []
+    1...10.times do |i|
+      resource = "https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=#{ENV['GNAVI_API_KEY']}&areacode_s=#{code}&hit_per_page=100&offset_page=#{i + 1}"
+      response = fetch_data(resource)
+      next if  response["error"]
+      response['rest'].each do |item|
+        restaurants.push(item)
+      end
+      if i == 9 && response['total_hit_count'] > 1000
+        puts "success"
+        1...10.times do |j|
+          resource_second = "https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=#{ENV['GNAVI_API_KEY']}&areacode_s=#{code}&hit_per_page=100&offset_page=#{10 + j + 1}"
+          response_second = fetch_data(resource_second)
+          puts response_second
+          next if response_second["error"]
+          response_second['rest'].each do |item|
+            restaurants.push(item)
+          end
+        end
+      end
     end
+    puts restaurants.length
+    #   restaurants_first['rest'].each do |restaurant|
+    #     restaurants_name = restaurant['name']
+    #     restaurants_id = restaurant['id']
+    #     place_id = get_place_id(restaurant_name)
+    #     reviews = get_reviews(place_id)
+    #     reviews.each do |review|
+    #       put_item(restaurants_id, place_id, reviews)
+    #     end
+    #   end
+    #   if i === 9 && restaurants_first['result']['total_hit_count'] > 1000
+    #     1...10.times do |j|
+    #       resource_second = "https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=#{ENV['GNAVI_API_KEY']}&areacode_s=#{code}&hit_per_page=100&offset_page=#{10 + j + 1}"
+    #     end
+    #   end
+    # end
+    # # if restaurants['result']['total_hit_count'] > 1000
+    # #   puts restaurants['result']['total_hit_count']
+    # # end
+    # # # for page in 1...100
+    # # restaurants = fetch_data(resource)
+    # # # puts restaurants
+    # # # # restaurants.each do |restaurant|
+
+    # end
   end
 end
 
